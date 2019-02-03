23f7412c55d08f1a348d20977015c5e7fc014028
GeoSn0w <GeoSn0w@users.noreply.github.com>
2019-02-03 03:24:44 +0100

    Create offsets.c

---
+++ offsets.c
@@ -0,0 +1,233 @@
+//GeoSn0w (C) GeoSn0w (@FCE365) 2019
+//Offsets.c
+
+//iOS 12.1.2 - iPhone X
+{ "12.1.1", "iPhone10,6", "D221AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,6", "D221AP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1.1 - iPhone X
+{ "12.1.1", "iPhone10,6", "D221AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,6", "D221AP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1 - iPhone X
+{ "12.1", "iPhone10,6", "D221AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone10,6", "D221AP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0.1 - iPhone X
+{ "12.0.1", "iPhone10,6", "D221AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,6", "D221AP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0 - iPhone X
+{ "12.0", "iPhone10,6", "D221AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,6", "D221AP", "_kernproc", 0xfffffff00766a0d8},
+
+
+//iOS 12.1.2 - iPhone 8 Plus
+{ "12.1.2", "iPhone10,5", "D211AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,5", "D211AP", "_kernproc", 0xfffffff0076660d8},
+{ "12.1.2", "iPhone10,5", "D211AAP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,5", "D211AAP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1.1 - iPhone 8 Plus
+{ "12.1.1", "iPhone10,5", "D211AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,5", "D211AP", "_kernproc", 0xfffffff0076660d8},
+{ "12.1.1", "iPhone10,5", "D211AAP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,5", "D211AAP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1 - iPhone 8 Plus
+{ "12.1", "iPhone10,5", "D211AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone10,5", "D211AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.1", "iPhone10,5", "D211AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone10,5", "D211AAP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0.1 - iPhone 8 Plus
+{ "12.0.1", "iPhone10,5", "D211AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,5", "D211AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.0.1", "iPhone10,5", "D211AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,5", "D211AAP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0 - iPhone 8 Plus
+{ "12.0", "iPhone10,5", "D211AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,5", "D211AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.0", "iPhone10,5", "D211AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,5", "D211AAP", "_kernproc", 0xfffffff00766a0d8},
+
+
+//iOS 12.1.2 - iPhone 8
+{ "12.1.2", "iPhone10,4", "D201AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,4", "D201AP", "_kernproc", 0xfffffff0076660d8},
+{ "12.1.2", "iPhone10,4", "D201AAP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,4", "D201AAP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1.1 - iPhone 8
+{ "12.1.1", "iPhone10,4", "D201AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,4", "D201AP", "_kernproc", 0xfffffff0076660d8},
+{ "12.1.1", "iPhone10,4", "D201AAP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,4", "D201AAP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.0.1 - iPhone 8
+{ "12.0.1", "iPhone10,4", "D201AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,4", "D201AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.0.1", "iPhone10,4", "D201AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,4", "D201AAP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0 - iPhone 8
+{ "12.0", "iPhone10,4", "D201AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,4", "D201AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.0", "iPhone10,4", "D201AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,4", "D201AAP", "_kernproc", 0xfffffff00766a0d8},
+
+
+//iOS 12.1.2 - iPhone X
+{ "12.1.2", "iPhone10,3", "D22AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,3", "D22AP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1.1 - iPhone X
+{ "12.1.1", "iPhone10,3", "D22AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,3", "D22AP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1 - iPhone X
+{ "12.1", "iPhone10,3", "D22AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone10,3", "D22AP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0.1 - iPhone X
+{ "12.0.1", "iPhone10,3", "D22AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,3", "D22AP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0 - iPhone X
+{ "12.0", "iPhone10,3", "D22AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,3", "D22AP", "_kernproc", 0xfffffff00766a0d8},
+
+
+//iOS 12.1.2 - iPhone 8 Plus
+{ "12.1.2", "iPhone10,2", "D21AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,2", "D21AP", "_kernproc", 0xfffffff0076660d8},
+{ "12.1.2", "iPhone10,2", "D21AAP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,2", "D21AAP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1.1 - iPhone 8 Plus
+{ "12.1.1", "iPhone10,2", "D21AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,2", "D21AP", "_kernproc", 0xfffffff0076660d8},
+{ "12.1.1", "iPhone10,2", "D21AAP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,2", "D21AAP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1 - iPhone 8 Plus
+{ "12.1", "iPhone10,2", "D21AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone10,2", "D21AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.1", "iPhone10,2", "D21AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone10,2", "D21AAP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0.1 - iPhone 8 Plus
+{ "12.0.1", "iPhone10,2", "D21AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,2", "D21AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.0.1", "iPhone10,2", "D21AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,2", "D21AAP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0.1 - iPhone 8 Plus
+{ "12.0", "iPhone10,2", "D21AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,2", "D21AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.0", "iPhone10,2", "D21AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,2", "D21AAP", "_kernproc", 0xfffffff00766a0d8},
+
+
+//iOS 12.1.2 - iPhone 8
+{ "12.1.2", "iPhone10,1", "D20AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,1", "D20AP", "_kernproc", 0xfffffff0076660d8},
+{ "12.1.2", "iPhone10,1", "D20AAP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.2", "iPhone10,1", "D20AAP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1.1 - iPhone 8
+{ "12.1.1", "iPhone10,1", "D20AP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,1", "D20AP", "_kernproc", 0xfffffff0076660d8},
+{ "12.1.1", "iPhone10,1", "D20AAP", "_rootvnode", 0xfffffff0076660c0},
+{ "12.1.1", "iPhone10,1", "D20AAP", "_kernproc", 0xfffffff0076660d8},
+//iOS 12.1 - iPhone 8
+{ "12.1", "iPhone10,1", "D20AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone10,1", "D20AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.1", "iPhone10,1", "D20AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone10,1", "D20AAP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0.1 - iPhone 8
+{ "12.0.1", "iPhone10,1", "D20AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,1", "D20AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.0.1", "iPhone10,1", "D20AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0.1", "iPhone10,1", "D20AAP", "_kernproc", 0xfffffff00766a0d8},
+//iOS 12.0 - iPhone 8
+{ "12.0", "iPhone10,1", "D20AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,1", "D20AP", "_kernproc", 0xfffffff00766a0d8},
+{ "12.0", "iPhone10,1", "D20AAP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.0", "iPhone10,1", "D20AAP", "_kernproc", 0xfffffff00766a0d8},
+
+
+//iOS 12.1.2 - iPhone 7 Plus
+{ "12.1.2", "iPhone9,4", "D111AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1.2", "iPhone9,4", "D111AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.1.1 - iPhone 7 Plus
+{ "12.1.1", "iPhone9,4", "D111AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1.1", "iPhone9,4", "D111AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.1 - iPhone 7 Plus
+{ "12.1", "iPhone9,4", "D111AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1", "iPhone9,4", "D111AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.0.1 - iPhone 7 Plus
+{ "12.0.1", "iPhone9,4", "D111AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.0.1", "iPhone9,4", "D111AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.0 - iPhone 7 Plus
+{ "12.0", "iPhone9,4", "D111AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.0", "iPhone9,4", "D111AP", "_kernproc", 0xfffffff0076420d0},
+
+
+//iOS 12.1.2 - iPhone 7
+{ "12.1.2", "iPhone9,3", "D101AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1.2", "iPhone9,3", "D101AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.1 - iPhone 7
+{ "12.1", "iPhone9,3", "D101AP", "_rootvnode", 0xfffffff00766a0c0},
+{ "12.1", "iPhone9,3", "D101AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.1.1 - iPhone 7
+{ "12.1.1", "iPhone9,3", "D101AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1.1", "iPhone9,3", "D101AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.0.1 - iPhone 7
+{ "12.0.1", "iPhone9,3", "D101AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.0.1", "iPhone9,3", "D101AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.0 - iPhone 7
+{ "12.0", "iPhone9,3", "D101AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.0", "iPhone9,3", "D101AP", "_kernproc", 0xfffffff0076420d0},
+
+
+//iOS 12.1.2 - iPhone 7 Plus
+{ "12.1.2", "iPhone9,2", "D11AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1.2", "iPhone9,2", "D11AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.1.1 - iPhone 7 Plus
+{ "12.1.1", "iPhone9,2", "D11AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1.1", "iPhone9,2", "D11AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.1 - iPhone 7 Plus
+{ "12.1", "iPhone9,2", "D11AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1", "iPhone9,2", "D11AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.0.1 - iPhone 7 Plus
+{ "12.0.1", "iPhone9,2", "D11AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.0.1", "iPhone9,2", "D11AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.0 - iPhone 7 Plus
+{ "12.0", "iPhone9,2", "D11AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.0", "iPhone9,2", "D11AP", "_kernproc", 0xfffffff0076420d0},
+
+
+//iOS 12.1.2 - iPhone 7
+{ "12.1.2", "iPhone9,1", "D10AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1.2", "iPhone9,1", "D10AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.1.1 - iPhone 7
+{ "12.1.1", "iPhone9,1", "D10AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1.1", "iPhone9,1", "D10AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.1 - iPhone 7
+{ "12.1", "iPhone9,1", "D10AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.1", "iPhone9,1", "D10AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.0.1 - iPhone 7
+{ "12.0.1", "iPhone9,1", "D10AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.0.1", "iPhone9,1", "D10AP", "_kernproc", 0xfffffff0076420d0},
+//iOS 12.0 - iPhone 7
+{ "12.0", "iPhone9,1", "D10AP", "_rootvnode", 0xfffffff0076420b8},
+{ "12.0", "iPhone9,1", "D10AP", "_kernproc", 0xfffffff0076420d0},
+
+
+//iOS 12.1.2 - iPhone SE
+{ "12.1.2", "iPhone8,4", "N69AP", "_rootvnode", 0xfffffff0076020b8},
+{ "12.1.2", "iPhone8,4", "N69AP", "_kernproc", 0xfffffff0076020d0},
+{ "12.1.2", "iPhone8,4", "N69uAP", "_rootvnode", 0xfffffff0076020b8},
+{ "12.1.2", "iPhone8,4", "N69uAP", "_kernproc", 0xfffffff0076020d0},
+//iOS 12.1.1 - iPhone SE
+{ "12.1.1", "iPhone8,4", "N69AP", "_rootvnode", 0xfffffff0076020b8},
+{ "12.1.1", "iPhone8,4", "N69AP", "_kernproc", 0xfffffff0076020d0}, 
+{ "12.1.1", "iPhone8,4", "N69uAP", "_rootvnode", 0xfffffff0076020b8},
+{ "12.1.1", "iPhone8,4", "N69uAP", "_kernproc", 0xfffffff0076020d0}, 
+//iOS 12.1 - iPhone SE
+{ "12.1", "iPhone8,4", "N69AP", "_rootvnode", 0xfffffff0076020b8}, 
+{ "12.1", "iPhone8,4", "N69AP", "_kernproc", 0xfffffff0076020d0},
+{ "12.1", "iPhone8,4", "N69uAP", "_rootvnode", 0xfffffff0076020b8}, 
+{ "12.1", "iPhone8,4", "N69uAP", "_kernproc", 0xfffffff0076020d0},
+//iOS 12.0.1 - iPhone SE
+{ "12.0.1", "iPhone8,4", "N69AP", "_rootvnode", 0xfffffff0076020b8},
+{ "12.0.1", "iPhone8,4", "N69AP", "_kernproc", 0xfffffff0076020d0},
+{ "12.0.1", "iPhone8,4", "N69uAP", "_rootvnode", 0xfffffff0076020b8},
+{ "12.0.1", "iPhone8,4", "N69uAP", "_kernproc", 0xfffffff0076020d0},
+//iOS 12.0 - iPhone SE
+{ "12.0", "iPhone8,4", "N69AP", "_rootvnode", 0xfffffff0076020b8},
+{ "12.0", "iPhone8,4", "N69AP", "_kernproc", 0xfffffff0076020d0},
+{ "12.0", "iPhone8,4", "N69uAP", "_rootvnode", 0xfffffff0076020b8},
+{ "12.0", "iPhone8,4", "N69uAP", "_kernproc", 0xfffffff0076020d0},
