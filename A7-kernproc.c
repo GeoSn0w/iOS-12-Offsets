c64d195f0d83751971bcb0c55fcc1958e709fe45
GeoSn0w <GeoSn0w@users.noreply.github.com>
2019-02-15 09:58:28 +0100

    Create A7-kernproc.c

---
+++ A7-kernproc.c
@@ -0,0 +1,27 @@
+
+//iPhone 5S
+uint64_t iPhone5S_12_0          = 0xfffffff0088c9f98;
+uint64_t iPhone5S_12_0_1        = 0xfffffff0088c9f98;
+uint64_t iPhone5S_12_1          = 0xfffffff0088d5f98;
+uint64_t iPhone5S_12_1_1        = 0xfffffff0088b5ef8;
+uint64_t iPhone5S_12_1_2        = 0xfffffff0088b5ef8;
+
+//iPod Touch 6
+uint64_t iPodTouch6_12_0        = 0xfffffff0089b3f98;
+uint64_t iPodTouch6_12_0_1      = 0xfffffff0089b3f98;
+uint64_t iPodTouch6_12_1        = 0xfffffff0089bbf98;
+uint64_t iPodTouch6_12_1_1      = 0xfffffff00899fef8;
+
+//iPhone 6
+uint64_t iPhone6_12_0           = 0xfffffff008a2ff98;
+uint64_t iPhone6_12_0_1         = 0xfffffff008a2ff98;
+uint64_t iPhone6_12_1           = 0xfffffff008a3ff98;
+uint64_t iPhone6_12_1_1         = 0xfffffff008a1fef8;
+uint64_t iPhone6_12_1_2         = 0xfffffff008a1fef8;
+
+//iPhone 6 Plus
+uint64_t iPhone6Plus_12_0       = 0xfffffff008a2ff98;
+uint64_t iPhone6Plus_12_0_1     = 0xfffffff008a2ff98;
+uint64_t iPhone6Plus_12_1       = 0xfffffff008a3ff98;
+uint64_t iPhone6Plus_12_1_1     = 0xfffffff008a1fef8;
+uint64_t iPhone6Plus_12_1_2     = 0xfffffff008a1fef8;
